ab7e6f1fe1b32517312d6d118c3602107d000b9e
legil <leslieg033@gmail.com>
2019-04-12 01:37:44 -0400

    project setup

--- .history/src/App_20190412013009.js
+++ .history/src/App_20190412013009.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         
       </div>
     );
